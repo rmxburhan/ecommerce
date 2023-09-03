e1d396138fa00936997947528144337e5ddd7c21
= <rizalburhanudin556@gmail.com>
2023-09-03 07:18:17 +0700

    Add Chart endpoint
    1. Add chart, 2. Update chart, 3. Delete chart, 4. Get chart list

---
+++ Ecommerce.Api/dto/chart/AddChartRequest.cs
@@ -0,0 +1,8 @@
+namespace Ecommerce.Api.dto.chart;
+
+public class AddChartRequest
+{
+    public int Qty { get; set; } = 1;
+    public string Notes { get; set; } = string.Empty;
+    public int ProductId { get; set; }
+}
